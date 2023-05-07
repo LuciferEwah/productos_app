c8c6e545521b24ba48e0e7fc37cd74d1943668d8
LuciferEwah <aramirezm@uft.edu>
2023-05-07 14:42:13 -0400

    expoort

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,5 @@
 export 'package:productos_app/models/product_model.dart';
 export 'package:productos_app/models/user_model.dart';
 export 'package:productos_app/models/venta_model.dart';
+export 'package:productos_app/models/suscripcion_model.dart';
+export 'package:productos_app/models/planes_model.dart';
--- lib/models/suscripcion_model.dart
+++ lib/models/suscripcion_model.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final suscripciones = suscripcionesFromJson(jsonString);
-
 import 'dart:convert';
 
 class Suscripciones {
