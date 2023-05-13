5d34f11c93fd9ea7d3e3961aa084c68d3f8a81d8
Poroto27 <112911588+Poroto27@users.noreply.github.com>
2023-05-12 23:33:58 -0400

    lista2.0

--- lib/screens/admin_sub.dart
+++ lib/screens/admin_sub.dart
@@ -8,59 +8,49 @@ class AdminSubScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /*
-    final userListProvider = Provider.of<UserListProvider>(context);
-    final users = userListProvider.users;*/
     final suscriptionListProvider = Provider.of<SuscriptionListProvider>(context);
+    final userListProvider = Provider.of<UserListProvider>(context);
     final suscriptions = suscriptionListProvider.suscripciones;
 
     return Scaffold(
       backgroundColor: Colors.white, // Added white background
       appBar: AppBar(
-        title: const Text('Suscripciones Activas'),
+        title: Text('Suscripciones Activas'),
       ),
       body: suscriptions.isEmpty
-          ? const Center(child: Text('No suscription found'))
+          ? const Center(child: Text('No hay suscripciones Activas'))
           : ListView.builder(
               itemCount: suscriptions.length,
               itemBuilder: (context, index) {
                 final suscription = suscriptions[index];
-                return Dismissible(
-                  key: UniqueKey(),
-                  background: Container(
-                    color: Colors.red,
-                    alignment: Alignment.centerRight,
-                    padding: const EdgeInsets.only(right: 16),
-                    child: const Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                      size: 36,
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey.shade400,
+                      width: 1,
                     ),
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                  direction: DismissDirection.endToStart,
-                  /*
-                  onDismissed: (_) {
-                    suscriptionListProvider.deleteById(user.id);
-                  },*/
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey.shade400,
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(8),
+                  child: ListTile(
+                    title: FutureBuilder<String?>(
+                      future: userListProvider.getEmailById(suscription.idUsuario!),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          final email = snapshot.data ?? '';
+                          return Text(email);
+                        } else {
+                          return const SizedBox.shrink();
+                        }
+                      },
                     ),
-                    child: ListTile(
-                      title: Text(suscription.estado),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('ID: ${suscription.id}'),
-                          Text('Fecha Inicio: ${suscription.fechaInicio}'),
-                          Text('Fecha Fin: ${suscription.fechaFin}'),
-                          Text('IdUsuario: ${suscription.idUsuario}'),
-                        ],
-                      ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('ID Plan: ${suscription.idPlan}'),
+                        Text('Fecha Inicio: ${suscription.fechaInicio}'),
+                        Text('Fecha Fin: ${suscription.fechaFin}'),
+                        Text('ID Usuario: ${suscription.idUsuario}'),
+                      ],
                     ),
                   ),
                 );
